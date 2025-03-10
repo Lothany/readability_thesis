fb9712d743f61e23b53202a62b0b36b7b6af0e09
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-03-10 14:53:16 +0800

    renamed test files for testing name and grade level extraction, extraction now working

--- run/prep_dataset.py
+++ run/prep_dataset.py
@@ -1,12 +1,13 @@
 import json
+from pathlib import Path
 from preprocess import open_file
 
 class DatasetEntry:
-    def __init__(self, chunk, stride_len, stride_index):
+    def __init__(self, chunk, stride_len, stride_index, text_num, grade_lvl):
         self.dictionary = self.load_dictionary(self)
         
-        # self.text_num = None
-        # self.grade_lvl = None
+        self.text_num = text_num
+        self.grade_lvl = grade_lvl
         self.chunk = chunk
         self.stride_len = stride_len
         self.stride_index = stride_index
@@ -41,28 +42,30 @@ class DatasetEntry:
         return float(f"{num:.4g}")
             
 
-def stride_n(file_path, n):    
+def stride_n(file_path):    
+    file = Path(file_path)
+    text_num = int(file.stem)
+    grade_lvl = int(file.parent.name[1:])
+    
     text = open_file(file_path)
     all_words = text.split()
     
-    words = [word for word in all_words if word not in {"$", "#"}]
-    n_gram = [words[i:i + n] for i in range(0, len(words) - n + 1)]
-
-    return n_gram, n
-
-def features(n_gram, n):
-    for stride_index, chunk in enumerate(n_gram):
-        entry = DatasetEntry(chunk, n, stride_index)
-        print(f"{entry.stride_len} | {entry.stride_index} | {entry.chunk} | {entry.noun_tr}")
-        
-        # Get traditional properties (chunk, stride_index)
-        
-def main():
-    text = "txt/cleaned/gtest/short.txt"
-    
     n_values = {2, 3, 5}
     for n in n_values:
-        n_gram, n = stride_n(text, n)
-        features(n_gram, n)
+        words = [word for word in all_words if word not in {"$", "#"}]
+        n_gram = [words[i:i + n] for i in range(0, len(words) - n + 1)]
+
+        features(n_gram, n, text_num, grade_lvl)
+    # return n_gram, n, text_num, grade_lvl
+
+def features(n_gram, n, text_num, grade_lvl):
+    for stride_index, chunk in enumerate(n_gram):
+        entry = DatasetEntry(chunk, n, stride_index, text_num, grade_lvl)
+        print(f"{entry.text_num} | {entry.grade_lvl} | {entry.stride_len} | {entry.stride_index} | {entry.chunk} | {entry.noun_tr}")
+        
+def main():
+    text = "txt/cleaned/g0/420.txt"
+    stride_n(text)
+        
     
 main()

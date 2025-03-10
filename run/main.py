8e1717df9a2e740fafaa5d6ac34a0f3b109c4dcd
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-03-10 15:24:51 +0800

    added positional marker for future addition of dictionary embedding at main file

--- run/main.py
+++ run/main.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     file = "txt/utf/g0/420.txt"
     
     normalized_path = run_normalize(file)
+    # Insert dictionary embeeding function here
     run_prep_dataset(normalized_path)
     

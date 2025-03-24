36c5315ceb4fcd11fa20702ee5abdd782ced5f49
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-03-24 16:58:24 +0800

    added models directory for machine learning models

---
+++ models/random_forest.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import warnings
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+warnings.filterwarnings('ignore')
+
+df= pd.read_csv('tables/dataset.csv')
+print(df)

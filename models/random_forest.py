6588f886aa5227adb5b8826258f93f4521206269
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-03-25 13:32:06 +0800

    implemented sample function from g4g

--- models/random_forest.py
+++ models/random_forest.py
@@ -2,18 +2,61 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
-import warnings
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import KNNImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import f1_score
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 
+import warnings
 warnings.filterwarnings('ignore')
 
+# Load dataset and extract variables
 df= pd.read_csv('tables/dataset.csv')
 print(df)
+
+X = df.drop(columns=['grade_level']).values
+y = df['grade_level'].values
+
+# print("Independent Variables (X):")
+# print(X)
+# print("\nDependent Variable (y):")
+# print(y)
+
+# Generating random forest
+label_encoder = LabelEncoder()
+x_categorical = df.select_dtypes(include=['object']).apply(label_encoder.fit_transform)
+x_numerical = df.select_dtypes(exclude=['object']).values
+x = pd.concat([pd.DataFrame(x_numerical), x_categorical], axis=1).values
+
+regressor = RandomForestRegressor(n_estimators=10, random_state=0, oob_score=True)
+regressor.fit(x, y)
+
+
+# Computing scores
+oob_score = regressor.oob_score_
+print(f'Out-of-Bag Score: {oob_score}')
+
+predictions = regressor.predict(x)
+
+mse = mean_squared_error(y, predictions)
+print(f'Mean Squared Error: {mse}')
+
+r2 = r2_score(y, predictions)
+print(f'R-squared: {r2}')
+
+X_grid = np.arange(min(X),max(X),0.01)
+X_grid = X_grid.reshape(len(X_grid),1) 
+  
+plt.scatter(X,y, color='blue') #plotting real points
+plt.plot(X_grid, regressor.predict(X_grid),color='green') #plotting for predict points
+  
+plt.title("Random Forest Regression Results")
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()

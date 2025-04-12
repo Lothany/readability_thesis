545d4147ea7561147730e06153394137a146875b
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-04-12 14:41:02 +0800

    changes to run.py for python3

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ cmd = ""
 
 if platform.system() == "Windows":
     activate_script = os.path.join("Scripts", "activate.bat")
-    cmd = f'cmd /k "python {script}"'
+    cmd = f'cmd /k "python3 {script}"'
 else:
     activate_script = os.path.join("bin", "activate")
-    cmd = f'/bin/bash -c "python {script}"'
+    cmd = f'/bin/bash -c "python3 {script}"'
 
 subprocess.run(cmd, shell=True)

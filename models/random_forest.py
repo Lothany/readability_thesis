6c7e0e3741f1cbb9222c2b104e6f6c215c1bb7ca
Ivy Cagaanan <ivyfranchezca.cagaanan@g.msuiit.ed.ph>
2025-04-23 15:02:47 +0800

    feat: random_forest compares model accuracy for different stride values

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,66 +1,143 @@
+# Data Processing
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn
 import numpy as np
 
-from sklearn.preprocessing import LabelEncoder
-from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import f1_score
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score
+# Modelling
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
 
-import warnings
-warnings.filterwarnings('ignore')
+# Tree Visualisation
+from sklearn.tree import export_graphviz
+from IPython.display import display
+from IPython.display import Image
+import graphviz
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load dataset and extract variables
-df= pd.read_csv('tables/dataset.csv')
-df = df[df['grade_level'] != 0]
 
-# To Do: Load testing dataset
+# Progress Bar
+from tqdm import tqdm
 
-print(df)
+# Filter dataset based on stride length
+def filter_dataset(dataset, stride_length):
+    # Return sentence fragments
+    if stride_length == -1:
+        filtered_dataset = dataset[(dataset['stride_len'] > 3) & (dataset['stride_len'] < 100)]
+    # Do not filter
+    elif stride_length == 0:
+        return dataset
+    else:
+        filtered_dataset = dataset[dataset['stride_len'] == stride_length]
+    
+    return filtered_dataset
 
-X = df.drop(columns=['grade_level', 'text_num', 'stride_index', 'n_gram', 'word_len']).values
-y = df['grade_level'].values
+def load_dataset(training_source, testing_source, stride_length):
+    train_dataset= pd.read_csv(training_source)
 
-print("Independent Variables (X):")
-print(X)
-print("\nDependent Variable (y):")
-print(y)
+    # Filter out rows with from 18.txt and drop empty column word_len
+    train_dataset = train_dataset[train_dataset['text_num'] != 18]
+    train_dataset = train_dataset.drop(columns=['word_num'])
 
-# Generating random forest
-label_encoder = LabelEncoder()
-x_categorical = df.select_dtypes(include=['object']).apply(label_encoder.fit_transform)
-x_numerical = df.select_dtypes(exclude=['object']).values
-x = pd.concat([pd.DataFrame(x_numerical), x_categorical], axis=1).values
+    # Execute for specific stride length
+    train_dataset = filter_dataset(train_dataset, stride_length)
 
-regressor = RandomForestRegressor(n_estimators=10, random_state=0, oob_score=True)
-regressor.fit(x, y)
+    # dataset['grade_level'] = dataset['grade_level'].map({'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6})
+    # print(train_dataset)
 
-# Computing scores
-oob_score = regressor.oob_score_
-print(f'Out-of-Bag Score: {oob_score}')
+    # Split the dataset into features and target variable
+    X_train = train_dataset.drop(columns=['grade_level', 'text_num', 'stride_len', 'stride_index', 'n_gram'])
+    y_train = train_dataset['grade_level']
 
-predictions = regressor.predict(x)
+    # Load testing dataset
+    test_dataset = pd.read_csv(testing_source)
+    test_dataset = test_dataset.drop(columns=['word_num'])
+    test_dataset = filter_dataset(test_dataset, stride_length)
 
-mse = mean_squared_error(y, predictions)
-print(f'Mean Squared Error: {mse}')
+    # test_dataset['grade_level'] = test_dataset['grade_level'].map({'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6})
+    # print(test_dataset)
 
-r2 = r2_score(y, predictions)
-print(f'R-squared: {r2}')
+    X_test = test_dataset.drop(columns=['grade_level', 'text_num', 'stride_len', 'stride_index', 'n_gram'])
+    y_test = test_dataset['grade_level']
+    
+    return X_train, y_train, X_test, y_test
+    
+# Train the Random Forest Classifier
+def train_model(X_train, y_train, X_test, y_test):
+    rf = RandomForestClassifier(n_estimators=0, warm_start=True, random_state=42)
+    n_trees = 100
 
-X_grid = np.arange(min(X),max(X),0.01)
-X_grid = X_grid.reshape(len(X_grid),1) 
-  
-plt.scatter(X,y, color='blue') #plotting real points
-plt.plot(X_grid, regressor.predict(X_grid),color='green') #plotting for predict points
-  
-plt.title("Random Forest Regression Results")
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
+    for i in tqdm(range(1, n_trees + 1), desc="Training Progress"):
+        rf.set_params(n_estimators=i)
+        rf.fit(X_train, y_train)
+    
+    return rf
+    
+def evaluate_model(X_train, y_train, X_test, y_test, model):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy}\n")
+    
+    cm = confusion_matrix(y_test, y_pred)
+    print(f"Confusion matrix:\n {cm}\n")
+    
+    feature_scores = pd.Series(model.feature_importances_, index=X_train.columns).sort_values(ascending=False)
+    print(f"Feature Importance:\n{feature_scores}\n")
+    
+    print(f"Classification Report:\n{classification_report(y_test, y_pred)}\n")
+    
+
+def feature_importance(X_train, y_train, X_test, y_test, model):
+    feature_scores = pd.Series(model.feature_importances_, index=X_train.columns).sort_values(ascending=False)
+    
+    sns.barplot(x=feature_scores, y=feature_scores.index)
+    plt.xlabel('Feature Importance Score')
+    plt.ylabel('Features')
+    plt.title("Visualizing Important Features")
+
+    plt.show()
+    
+def model_accuracy():
+    training_source = 'tables/dataset.csv'
+    testing_source = 'tables/dataset_testing.csv'
+    
+    for stride in [-1, 0, 1, 2, 3, 100]:    
+        X_train, y_train, X_test, y_test = load_dataset(training_source, testing_source, stride)
+        model = train_model(X_train, y_train, X_test, y_test)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        if stride == 0:
+            print("No stride length filter applied.")
+        elif stride == -1:
+            print("Sentence fragments.")
+        else:
+            print(f"Stride length: {stride}")
+        
+        print(f"Accuracy Score: {accuracy}\n")
+        
+    
+def main():
+    # Input desired n-gram length [1, 2, 3, 100]
+    # Enter 0 to skip filter and -1 to return sentence fragments
+    stride_length = 100
+    
+    if stride_length == 0:
+        print("No stride length filter applied.")
+    elif stride_length == -1:
+        print("Returning sentence fragments.")
+    elif stride_length in [1, 2, 3, 100]:
+        print(f"Filtering dataset with stride length: {stride_length}")
+    else:
+        print(f"Invalid stride length: {stride_length}")
+        return
+    
+    training_source = 'tables/dataset.csv'
+    testing_source = 'tables/dataset_testing.csv'
+    
+    X_train, y_train, X_test, y_test = load_dataset(training_source, testing_source, stride_length)
+    model = train_model(X_train, y_train, X_test, y_test)
+    # evaluate_model(X_train, y_train, X_test, y_test, model)
+    # feature_importance(X_train, y_train, X_test, y_test, model)
+    
+model_accuracy()
